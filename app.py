af8326fcc88369580dc4f6d42a81df525c21bd5f
rafi4482 <rafi.kazla@gmail.com>
2024-10-20 07:50:21 +0600

    done

--- app.py
+++ app.py
@@ -37,12 +37,12 @@ st.markdown("---")
 
 with st.form("input_form", clear_on_submit=False):
     st.subheader("Enter the following details:")
-    col1, col2, col3 = st.columns(3, gap="medium")
+    col1, col2, col3 = st.columns(3, gap="large")
 
     with col1:
         gender = st.selectbox("Gender", ["Male", "Female"], key="gender")
-        hypertension = st.selectbox("Hypertension", [0, 1], key="hypertension")
-        heart_disease = st.selectbox("Heart Disease", [0, 1], key="heart_disease")
+        hypertension = st.selectbox("Hypertension", ["Yes", "No"], key="hypertension")
+        heart_disease = st.selectbox("Heart Disease", ["Yes", "No"], key="heart_disease")
 
     with col2:
         age = st.number_input("Age", min_value=0, max_value=120, value=30, step=1, format="%d", key="age")
@@ -59,14 +59,29 @@ with st.form("input_form", clear_on_submit=False):
 
     col1, col2, col3 = st.columns([1, 1, 1])
     with col2:
+        st.markdown(
+            """
+            <style>
+            div.stButton > button {
+                margin-top: 20px;
+                margin-bottom: 20px;
+                margin-left: auto;
+                margin-right: auto;
+                display: block;
+                width: 50%;
+            }
+            </style>
+            """,
+            unsafe_allow_html=True
+        )
         submitted = st.form_submit_button("💡 Predict")
 
     if submitted:
         input_data = pd.DataFrame({
             "gender": [1 if gender == "Male" else 0],
             "age": [age],
-            "hypertension": [hypertension],
-            "heart_disease": [heart_disease],
+            "hypertension": [1 if hypertension == "Yes" else 0],
+            "heart_disease": [1 if heart_disease == "Yes" else 0],
             "smoking_history": [{
                 "never": 0, "No Info": 1, "former": 2,
                 "current": 3, "ever": 4, "not current": 5
